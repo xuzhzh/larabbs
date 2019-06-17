c1bd06efa26d6046da4b80cbd7e66ca7e66a65a8
山边的诗与风1 <602678598@qq.com>
2019-06-17 14:32:09 +0800

    剪裁头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,11 +1,12 @@
 <?php
 namespace APP\Handlers;
+use Image;
 class ImageUploadHandler{
 
     //只允许以下后罪名的图片我呢见上传
     protected $allowed_ext = ['png','jpg','gif','jpeg'];
 
-    public function save($file,$folder,$file_prefix)
+    public function save($file,$folder,$file_prefix,$max_width = false)
     {
         //构建存储文件夹规则
         //文件夹切割能让查找效率
@@ -27,14 +28,35 @@ class ImageUploadHandler{
             return false;
         }
 
+
+
         //将图片移动到我们目标存储路径中
 
         $file->move($upload_path,$filename);
+        if ($max_width && $extension != 'gif') {
+
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
 
         return [
             'path' =>config('app.url')."/$folder_name/$filename"
         ];
 
 
+    }
+    public function reduceSize($file_path,$max_width)
+    {
+        $image=Image::make($file_path);
+
+        $image->resize($max_width,null,function($constraint){
+           // 设定宽度是 $max_width，高度等比例双方缩放
+           $constraint->aspectRatio();
+
+           // 防止裁图时图片尺寸变大
+           $constraint->upsize();
+        });
+         // 对图片修改后进行保存
+         $image->save();
     }
 }
