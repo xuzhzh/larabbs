202fe571eb512b5988dc87d4b908b1b61fa370fc
山边的诗与风1 <602678598@qq.com>
2019-06-18 10:51:11 +0800

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+use App\Models\Topic;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
@@ -37,4 +38,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+  
+    }
 }
