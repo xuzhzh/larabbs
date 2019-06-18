b95e3c1df839d6c1454700bd4b609b58c15fa08c
山边的诗与风1 <602678598@qq.com>
2019-06-18 10:37:41 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,31 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query,$order)
+    {
+        //不同的排序，使用不同的数据读取逻辑
+        switch($order){
+            case 'recent':
+            $query->recent();
+            break;
+
+            default:
+            $query->recentReplied();
+            break;
+        }
+        //预加载 防止N+1问题
+        return $query->with('user','category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        //当话题有新回复时，我们将编写逻辑来更新
+        return $query->orderBy('updated_at','desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at','desc');
+    }
 }
